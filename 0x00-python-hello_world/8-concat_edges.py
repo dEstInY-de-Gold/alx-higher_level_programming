944b7ae6acb4a55dd848fb58f048308b142ee46e
dEstInY-de-Gold <noahayebu@gmail.com>
2022-11-29 02:31:06 +0000

    create sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted language, interactive, object-oriented programming\
         langauge that combines remarkable power with very clear syntax"
-str = str[48:76] + 'with python'
+str = 'object-oriented programming with python'
 print(str)
