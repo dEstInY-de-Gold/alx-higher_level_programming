2f0a6f1e8b4fd523f60ca18e5e8ec0e5b48f19a1
dEstInY-de-Gold <noahayebu@gmail.com>
2022-11-30 05:46:52 +0000

    The bull is faat!

---
+++ 0x01-python-if_else_loops_functions/100-print_tebalpha.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for i in range(122, 96, -1):
+    c = ''
+    if i % 2 == 0:
+        c = chr(i)
+    else:
+        c = chr(i - 32)
+    print('{}'.format(c), end = '')
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    new_str = str[:]
+    if n >= 0:
+        new_str = str[:n] + str[n+1:]
+    return new_str
