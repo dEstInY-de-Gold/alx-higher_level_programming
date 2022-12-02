3802e0ff9ab944835913309f3a6f3b61401de8b5
dEstInY-de-Gold <noahayebu@gmail.com>
2022-12-02 04:08:42 +0000

    modules modified

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import calculator_1 as calc
 
-a = 10
-b = 5
-add = calc.add(a, b)
-sub = calc.sub(a, b)
-mul = calc.mul(a, b)
-div = calc.div(a, b)
-print('{} + {} = {}'.format(a, b, add))
-print('{} - {} = {}'.format(a, b, sub))
-print('{} * {} = {}'.format(a, b, mul))
-print('{} / {} = {}'.format(a, b, div))
+if __name__ == '__main__':
+    a = 10
+    b = 5
+    add = calc.add(a, b)
+    sub = calc.sub(a, b)
+    mul = calc.mul(a, b)
+    div = calc.div(a, b)
+    print('{} + {} = {}'.format(a, b, add))
+    print('{} - {} = {}'.format(a, b, sub))
+    print('{} * {} = {}'.format(a, b, mul))
+    print('{} / {} = {}'.format(a, b, div))
