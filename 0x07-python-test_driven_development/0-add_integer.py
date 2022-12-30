0a87bff7e7df4fb5af2f91f75c04686e9215ae7e
dEstInY-de-Gold <noahayebu@gmail.com>
2022-12-30 07:06:19 +0000

    test driver

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+'''
+A function that adds two integers
+'''
 
+
+def add_integer(a, b=98):
+    '''
+    Adds two integers
+    Args:
+        a: first argument
+        b: second argument
+    Returns: integer
+    '''
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError('a must be an integer')
+    elif not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError('b must be an integer')
+    else:
+        return int(a) + int(b)
