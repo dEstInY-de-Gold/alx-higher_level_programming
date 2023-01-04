ad6ca54403a4a7e7b50af87d4fae25bff4b61c4f
dEstInY-de-Gold <noahayebu@gmail.com>
2023-01-04 21:33:12 +0000

    python hello world

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -6,3 +6,12 @@ middle_word = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
+
+demo = word[1:-1]
+print('demo(1:-1): ', demo)
+print('word[-1]: ', word[-1])
+# 0 1 2 3 4 5 6 7 8
+# H o l b e r t o n
+
+# -1 -2 -3 -4 -5 -6 -7 -8 -9
+# n  o  t  r  e  b  l  o  H
