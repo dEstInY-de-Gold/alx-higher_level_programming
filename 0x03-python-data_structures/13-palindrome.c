845e6cd4c87473cf2cf5f7c79bb89edccc5ef7e3
dEstInY-de-Gold <noahayebu@gmail.com>
2023-01-26 01:30:51 +0000

    inherited

--- 0x03-python-data_structures/13-palindrome.c
+++ 0x03-python-data_structures/13-palindrome.c
@@ -3,4 +3,15 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *ptr;
+
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+	{
+		printf("Malloc error!!\n");
+		return (1);
+	}
+	while (head)
+	{
+		head = head->next;
+	}
 }
