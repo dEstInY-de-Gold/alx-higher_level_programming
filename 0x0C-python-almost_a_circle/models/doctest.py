c35b92e793a5a8639e16f91c6950ffe44a511e06
Noah <noahayebu@gmail.com>
2023-04-25 23:10:09 +0000

    py docs done

--- 0x0C-python-almost_a_circle/models/doctest.py
+++ 0x0C-python-almost_a_circle/models/doctest.py
@@ -1,7 +1,73 @@
 #!/usr/bin/python3
-from models.base import Base
+
+from base import Base
+
+from rectangle import Rectangle
+from square import Square
+import re
+
+
+def methods_base():
+    print('BASE ' * 5)
+    print(Base.__doc__)
+    funcs = ('__init__', 'to_json_string', 'save_to_file', 'from_json_string', 'create', 'load_from_file')
+    func_dic = {
+            '__init__': Base.__init__,
+            'to_json_string': Base.to_json_string,
+            'save_to_file': Base.save_to_file,
+            'from_json_string': Base.from_json_string,
+            'create': Base.create,
+            'load_from_file': Base.load_from_file
+            }
+    for func in funcs:
+        if func in func_dic:
+            print('*' * 15, func, '*' * 15)
+            print(func_dic[func].__doc__)
+       #     print(func.__doc__)
+
+def methods_rect():
+    print('RECTANGLE ' * 5)
+    funcs = ('__init__', 'width', 'height', 'x', 'y', 'area', 'display', '__str__', 'update', 'to_dictionary')
+    func_dic = {
+            '__init__': Rectangle.__init__,
+            'width': Rectangle.width,
+            'height': Rectangle.height,
+            'x': Rectangle.x,
+            'y': Rectangle.y,
+            'area': Rectangle.area,
+            'display': Rectangle.display,
+            '__str__': Rectangle.__str__,
+            'update': Rectangle.update,
+            'to_dictionary': Rectangle.to_dictionary
+            }
+    for func in funcs:
+        if func in func_dic:
+            print('*' * 15, func, '*' * 15)
+            print(func_dic[func].__doc__)
+
+def methods_square():
+    print('SQUARE ' * 5)
+    funcs = ('__init__', 'size', '__str__', 'update', 'to_dictionary')
+    func_dic = {
+            '__init__': Square.__init__,
+            'size': Square.size,
+            '__str__': Square.__str__,
+            'update': Square.update,
+            'to_dictionary': Square.to_dictionary
+            }
+    for func in funcs:
+        if func in func_dic:
+            print('*' * 15, func, '*' * 15)
+            print(func_dic[func].__doc__)
 
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod(Base)
+    methods_base()
+    print()
+    print('#*#' * 25)
+    print()
+    methods_rect()
+    print()
+    print('#*#' * 25)
+    print()
+    methods_square()
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
+from rectangle import Rectangle
+
 '''square Class'''
-from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    '''Square Class'''
+    '''Square Class
+    This defines a square object that inherits from the
+    rectangle class which also inherits from Base model.
+    It calls to the super init of rectangle with arguments; size,
+    x=0, y=0 and id=None.
+    Methods:
+        size: A setter and getter methods for size of square
+        __str__: A string representation for square object
+        update: Assigning values for each attribute
+        to_dictionary: Returns a dictionary representation of square object.
+    '''
+
     def __init__(self, size, x=0, y=0, id=None):
-        '''Inicilizating ATRIBUTES
-        Inheriting attributes ID, width, height, x, y'''
+        '''Inicilizing ATRIBUTES
+        Inheriting attributes ID, width, height, x, y
+        Args:
+            size (int): size of square
+            x (int, optional): horizontal position of square
+            y (int, optional): vertical positionn of square
+            id (int, optional): unique identifier for object.
+        '''
         super().__init__(size, size, x, y, id)
 
         self.width = size
@@ -15,12 +33,22 @@ class Square(Rectangle):
 
     @property
     def size(self):
-        # Preguntar por que es self.width si
-        # no estamos definiendo ese parametro
+        '''
+        Getter method for size
+        Returns:
+            int: size of the square object.
+        '''
         return self.width
 
     @size.setter
     def size(self, a):
+        '''
+        Setter method for size
+        Args (int): size of each side of square object.
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if size is less than one.
+        '''
         if not isinstance(a, int):
             raise TypeError('size must be an integer')
         else:
@@ -31,15 +59,21 @@ class Square(Rectangle):
                 self.height = a
 
     def __str__(self):
-        '''Method that returns
-        [Square] (<id>) <x>/<y> - <size>'''
+        '''Method that returns a string representation of square object
+        Returns:
+            (str):[Square] (<id>) <x>/<y> - <size>
+        '''
         t1 = "[Square]"
         t2 = " ({}) {}/{}".format(self.id, self.x, self.y)
         t3 = " - {}".format(self.width)
         return (t1 + t2 + t3)
 
     def update(self, *args, **kwargs):
-        '''Method that assigns a value to each attribute'''
+        '''Method that assigns a value to each attribute
+        Args:
+            args (list): A variable list of arguments.
+            kwargs (dic): A variable list of keyword arguments.
+        '''
         key = ["id", "size", "x", "y"]
         if len(args) > 0:
             for x in range(len(args)):
@@ -49,7 +83,10 @@ class Square(Rectangle):
                 setattr(self, a, kwargs[a])
 
     def to_dictionary(self):
-        '''Method that returns the dictionary representation of a Square'''
+        '''Method that returns the dictionary representation of a Square
+        Returns:
+            (dic): A key:value pairs of attributes.
+        '''
         key = ["id", "size", "x", "y"]
         value = [self.id, self.size, self.x, self.y]
         return dict(zip(key, value))
