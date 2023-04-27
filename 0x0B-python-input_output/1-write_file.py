669ced95a3bee1c4008b50029b311f73f9becd8b
Noah <noahayebu@gmail.com>
2023-04-27 01:34:28 +0000

    debugged read file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def write_file(filename="", text=""):
     '''
     Overwrites contents of a file
@@ -9,6 +10,4 @@ def write_file(filename="", text=""):
     '''
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
-       # return len(text)
     f.close()
-
--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def to_json_string(my_obj):
     '''
     json rep of object
