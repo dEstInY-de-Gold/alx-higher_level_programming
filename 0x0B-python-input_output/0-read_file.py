eb240219e747d61ebecae35309a1a75f998bd6c2
Noah <noahayebu@gmail.com>
2023-04-27 01:50:34 +0000

    files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,9 +4,10 @@
 def read_file(filename=""):
     '''
     Reads contents of file
+    Accepts filename as a parameter, opens it and
+    prints the output to stout.
     Args:
         filename: given file to read from
     '''
     with open(filename, 'r') as f:
         print(f.read())
-    f.close()
