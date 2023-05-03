efb186163573aac2c89222c0c940830d242f3b88
Noah <noahayebu@gmail.com>
2023-05-03 04:37:14 +0000

    debugging lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,7 +2,10 @@
 
 
 def lookup(obj):
-    '''
+    """
     returns the list of available attributes and methods of an object.
-    '''
+    Args:
+        obj (dic): A dictionary object as input.
+    Returns: (list): A list of available attributes and methods of an object.
+    """
     return dir(obj)
