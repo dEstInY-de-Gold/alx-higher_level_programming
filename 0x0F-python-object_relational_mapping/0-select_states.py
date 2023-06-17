1871b85c483bb338b378a25395979a2b97a26070
Noah <noahayebu@gmail.com>
2023-06-17 13:59:16 +0000

    made 0-select_states.py non-executable when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,36 +1,41 @@
 #!/usr/bin/python3
 
+'''
+List all states from database.
+Format: ./0-select_states.py <mysql username> <mysql password> <database name>
+'''
+
 import MySQLdb
 import sys
 
-# Get MySQL connection details from command line arguments
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
+if __name__ == '__main__':
+    # Get MySQL connection details from command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-# Connect to MySQL server
-db = MySQLdb.connect(
-    host='localhost',
-    port=3306,
-    user=mysql_username,
-    passwd=mysql_password,
-    db=database_name
-)
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
 
-# Create a cursor object to interact with the database
-cursor = db.cursor()
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
 
-# Execute the query to select all states and sort by states.id
-query = "SELECT * FROM states ORDER BY states.id ASC"
-cursor.execute(query)
+    # Execute the query to select all states and sort by states.id
+    query = "SELECT * FROM states ORDER BY states.id ASC"
+    cursor.execute(query)
 
-# Fetch all the rows returned by the query
-rows = cursor.fetchall()
+    # Fetch all the rows returned by the query
+    rows = cursor.fetchall()
 
-# Display the results
-for row in rows:
-    print(row)
+    # Display the results
+    for row in rows:
+        print(row)
 
-# Close the database connection
-db.close()
-
+    # Close the database connection
+    db.close()
