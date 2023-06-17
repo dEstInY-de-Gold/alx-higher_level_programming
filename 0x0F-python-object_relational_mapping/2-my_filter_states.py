b0a8651f3a6259a00633164838b6f72a754ceacd
Noah <noahayebu@gmail.com>
2023-06-17 14:19:37 +0000

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 '''
-A script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
-Format: python3 list_states.py <username> <password> <database_name>
+A script that takes in an argument and displays all \
+      values in the states table of hbtn_0e_0_usa \
+      where name matches the argument.
+Format: python search_states.py <username> <password> \
+        <database_name> <state_name>
 '''
 
 import sys
@@ -13,6 +16,7 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    state_name = sys.argv[4]
 
     # Connect to the MySQL server
     db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
@@ -20,17 +24,16 @@ if __name__ == '__main__':
     # Create a cursor object to interact with the database
     cursor = db.cursor()
 
-    # Execute the SQL query to fetch states starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cursor.execute(query)
+    # Execute the SQL query to fetch states matching the provided name
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
 
     # Fetch all the rows returned by the query
     rows = cursor.fetchall()
 
     # Display the results
     for row in rows:
-        if row[1][0] == 'N':
-            print(row)
+        print(row)
 
     # Close the cursor and database connection
     cursor.close()
