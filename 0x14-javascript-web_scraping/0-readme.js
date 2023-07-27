555c59108152d1d8c7c5808d72019e6b2954cf1b
dEstInY-de-Gold <noahayebu@gmail.com>
2023-07-27 03:33:09 +0000

    read and write files

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const ps = require('fs');
+const filepath = process.argv.slice(2);
+
+ps.readFile(filepath[0], 'utf8', (err, data) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	console.log(data);
+});
---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const ps = require('fs');
+const args = process.argv.slice(2);
+
+ps.writeFile(args[0], args[1], 'utf8', (err) => {
+	if (err) {
+		console.error();
+		return;
+	}
+});
