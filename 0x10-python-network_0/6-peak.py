cce7a5e289e699f1ed4bc1326d0075a56f2897a8
dEstInY-de-Gold <noahayebu@gmail.com>
2023-08-14 00:51:41 +0000

    modified peak finder

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
+'''
+Peak in a list
+'''
+
 def find_peak(list_of_integers):
     '''
     A function for finding the peak of a list
+    '''
     '''
     if len(list_of_integers) > 0:
         max = list_of_integers[0]
@@ -11,4 +16,21 @@ def find_peak(list_of_integers):
                 max = item
     else:
         max = None
-    return max
+        '''
+    if len(list_of_integers) == 0:
+        return None
+    
+    for item in list_of_integers:
+        indx = list_of_integers.index(item)
+        if indx < 1:
+            prev_item = 0
+        else:
+            prev_item = list_of_integers[indx-1]
+
+        if (len(list_of_integers) - 1) == indx:
+            next_item = 0
+        else:
+            next_item = list_of_integers[indx+1]
+        if item >= prev_item and item >= next_item:
+            pk = item
+    return pk
