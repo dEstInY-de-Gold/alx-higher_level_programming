7a01915ca56195889479b7176504b1ff1662bf83
Noah <noahayebu@gmail.com>
2024-01-26 04:18:15 +0000

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-
-# Check if the user provided a URL as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Store the URL provided by the user
-url="$1"
-
-# Use curl to send a GET request and store the output in a variable
-response=$(curl -sI "$url")
-
-# Extract the Content-Length header value from the response headers
-content_length=$(echo "$response" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r')
-
-# Use curl again to get the response body and measure its size in bytes
-body_size=$(curl -s "$url" | wc -c)
-
-# Display the content length in bytes
-echo "$body_size"
+# Using curl to get the response body and measure its size in bytes
+curl -s "$url" | wc -c
