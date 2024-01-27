a87291d32f091132b60924aa89ad39c0cb347c51
Noah <noahayebu@gmail.com>
2024-01-27 15:06:00 +0000

    bugs are snacky

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,7 +9,6 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     try:
-        #req = Request(url)
         with urlopen(url) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
